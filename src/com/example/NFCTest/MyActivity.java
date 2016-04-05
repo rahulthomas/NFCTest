ce9791aaa8d9414e1bde977889660efad890bed7
rahulthomas <g00275669@gmit.ie>
2016-04-05 14:38:47 +0100

    Initial commit for the test app to read nfc tags

---
+++ src/com/example/NFCTest/MyActivity.java
@@ -0,0 +1,98 @@
+package com.example.NFCTest;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcAdapter.CreateNdefMessageCallback;
+import android.nfc.NfcAdapter.OnNdefPushCompleteCallback;
+import android.nfc.NfcEvent;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MyActivity extends Activity implements
+        CreateNdefMessageCallback, OnNdefPushCompleteCallback {
+    TextView textInfo;
+    EditText textOut;
+
+    NfcAdapter nfcAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        textInfo = (TextView) findViewById(R.id.info);
+        textOut = (EditText) findViewById(R.id.textout);
+
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter == null) {
+            Toast.makeText(MyActivity.this,
+                    "nfcAdapter==null, no NFC adapter exists",
+                    Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(MyActivity.this,
+                    "Set Callback(s)",
+                    Toast.LENGTH_LONG).show();
+            nfcAdapter.setNdefPushMessageCallback(this, this);
+            nfcAdapter.setOnNdefPushCompleteCallback(this, this);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        if (action.equals(NfcAdapter.ACTION_NDEF_DISCOVERED)) {
+            Parcelable[] parcelables =
+                    intent.getParcelableArrayExtra(
+                            NfcAdapter.EXTRA_NDEF_MESSAGES);
+            NdefMessage inNdefMessage = (NdefMessage) parcelables[0];
+            NdefRecord[] inNdefRecords = inNdefMessage.getRecords();
+            NdefRecord NdefRecord_0 = inNdefRecords[0];
+            String inMsg = new String(NdefRecord_0.getPayload());
+            textInfo.setText(inMsg);
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        setIntent(intent);
+    }
+
+    @Override
+    public void onNdefPushComplete(NfcEvent event) {
+
+        final String eventString = "onNdefPushComplete\n" + event.toString();
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(),
+                        eventString,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+
+        String stringOut = textOut.getText().toString();
+        byte[] bytesOut = stringOut.getBytes();
+
+        NdefRecord ndefRecordOut = new NdefRecord(
+                NdefRecord.TNF_MIME_MEDIA,
+                "text/plain".getBytes(),
+                new byte[]{},
+                bytesOut);
+
+        NdefMessage ndefMessageout = new NdefMessage(ndefRecordOut);
+        return ndefMessageout;
+    }
+}
